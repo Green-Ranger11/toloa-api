cab3cf9aa247a3ace5eb8cd9faaf538676d917fa
Andre Westerlund <andrewest_04@hotmail.com>
2022-04-05 17:25:17 +1300

    generate user CRUD and DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { TopicModule } from './topic/topic.module';
 import { DiscussionModule } from './discussion/discussion.module';
 import { CollaborationModule } from './collaboration/collaboration.module';
 import { CommentModule } from './comment/comment.module';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CountryModule, TopicModule, DiscussionModule, CollaborationModule, CommentModule],
+  imports: [CountryModule, TopicModule, DiscussionModule, CollaborationModule, CommentModule, UserModule ],
   controllers: [AppController],
   providers: [AppService],
 })
