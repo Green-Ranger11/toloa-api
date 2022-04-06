53ff9e295a2ea3e1a331eadbcf1eb33c5a037eb7
Andre Westerlund <andrewest_04@hotmail.com>
2022-04-06 17:01:32 +1300

    Add CORS to Application. Solves Issue #6

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
