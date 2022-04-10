f8428c6f36d023a6f2155326a66f4e515ce78492
Andre Westerlund <andrewest_04@hotmail.com>
2022-04-10 17:55:55 +1300

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Toloa API')
