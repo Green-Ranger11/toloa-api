bcf7595ebc284809bbedbc9b5cef7310082acb00
Green-Ranger11 <junior.eteuati@gmail.com>
2022-04-12 14:56:13 +1300

    Rename swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(5000);
 }
